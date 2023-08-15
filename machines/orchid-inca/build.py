6b9adf783c651c16e74f7c1bd8623dacc4b6c608
Robin Chew <me@robin.com.au>
2023-08-15 23:46:36 +0800

    build.py script that builds package tree of specified apps.

--- machines/orchid-inca/build.py
+++ machines/orchid-inca/build.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import importlib
 from importlib.machinery import SourceFileLoader
 import os
@@ -26,14 +27,25 @@ def flatten_paths(d, l = ()):
             flatten_paths(v, l + (k,))
             for k, v in d.items()
         ]
-    return l
+    return l, d
+
+def flatten_lists(l):
+    return reduce(
+        lambda acc, item:
+            acc + (flatten_lists(item) if type(item) is list else [item]),
+        l,
+        [])
 
 if __name__ == '__main__':
     apps = SourceFileLoader("apps", os.environ['APPS_PY']).load_module()
+    build_folder = os.environ['BUILD_FOLDER']
 
     for package_name, build in get_builds():
         args = (package_name, apps.apps)
         tree = build.get_package_tree(*args)
-        from pprint import pprint
-        pprint(walk(tree, args), indent=4)
-        pprint(flatten_paths(walk(tree, args)), indent=4)
+
+        for path, content in flatten_lists(flatten_paths(walk(tree, args))):
+            full_path = (build_folder, package_name, *path)
+            os.makedirs(os.path.join(*full_path[0:-1]), exist_ok=True)
+            with open(os.path.join(*full_path), 'w') as f:
+                f.write(content)
